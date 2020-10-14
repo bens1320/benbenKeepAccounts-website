28f6cc34ec7cbaac2a14228f913b80e6d251f046
bens1320 <yu1piao09@163.com>
2020-10-15 00:58:57 +0800

    deploy

---
+++ precache-manifest.f6a466d296f7e0600c56f682874d707a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2b41d280dcfa0b6b42",
+    "url": "/benbenKeepAccounts-website/css/app.f84bc35d.css"
+  },
+  {
+    "revision": "7f7ac62b23b7f9f97968e86a06a73226",
+    "url": "/benbenKeepAccounts-website/index.html"
+  },
+  {
+    "revision": "3a2b41d280dcfa0b6b42",
+    "url": "/benbenKeepAccounts-website/js/app.f048324d.js"
+  },
+  {
+    "revision": "5fa2178efde57490d055",
+    "url": "/benbenKeepAccounts-website/js/chunk-vendors.b4bc1fdf.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/benbenKeepAccounts-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/benbenKeepAccounts-website/robots.txt"
+  }
+]);
